09db867c7b97db3c24fdba0b60a5493bb7c8f6f4
trentrash <trentrash.y@gmail.com>
2021-08-02 02:07:54 +0900

    Initial Commit

---
+++ RPS_Game/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  RPS_Game
+//
+//  Created by trentrash on 2021/08/02.
+//
+
+import Foundation
+
+func start() {
+    print("RPS_Game (開始:s / 終了:e)")
+    let Text:String = readLine() ?? ""
+    
+    switch Text {
+        case "s":
+            print("ゲームを開始します。")
+        case "e":
+            print("プログラムを終了します。")
+        case "":
+            print("どちらかを選択してください。")
+            return start()
+        default:
+            print("入力した値が合っていません。")
+            return start()
+    }
+}
+start()
