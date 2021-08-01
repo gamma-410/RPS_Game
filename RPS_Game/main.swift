5d82a69b24706077ca985c3bd74bc5b66071c171
trentrash <trentrash.y@gmail.com>
2021-08-02 02:36:07 +0900

    テスト

--- RPS_Game/main.swift
+++ RPS_Game/main.swift
@@ -9,12 +9,10 @@ import Foundation
 
 
 class RPS_Game {
-    
     // Game_run MainFunc()
     func Game_run(){
         
     }
-
     // Game_run StartFunc()
     func start() {
         print("RPS_Game (開始:s / 終了:e)")
@@ -31,7 +29,6 @@ class RPS_Game {
                 return start()
         }
     }
-    
 }
 
 var game = RPS_Game()
