d5ed7136ab5b28118a92fd2a54eb9c519102e80e
moka <moka.drip.web@gmail.com>
2021-08-02 22:48:02 +0900

    とりあえずテキストを英語化しました。グローバルですね。

--- RPS_Game/main.swift
+++ RPS_Game/main.swift
@@ -13,37 +13,37 @@ class RPS_Game {
     // Game_run MainFunc()
     func Game_run(){
         
-        print(">> ✊(r)✋(p)✌️(s)を選択してください。(終了:e)")
+        print(">> Select ✊(r) ✋(p) ✌️(s). (End: e)")
         let user_answer_before = readLine()
         
         switch user_answer_before {
         case "r":
             let user_answer_after = user_answer_before?.replacingOccurrences(of: "r", with: "✊")
             let new_answer = user_answer_after ?? ""
-            print("あなた: \(new_answer)を出しました。")
+            print("You: \(new_answer)")
             
         case "p":
             let user_answer_after = user_answer_before?.replacingOccurrences(of: "p", with: "✋")
             let new_answer = user_answer_after ?? ""
-            print("あなた: \(new_answer)を出しました。")
+            print("You: \(new_answer)")
             
         case "s":
             let user_answer_after = user_answer_before?.replacingOccurrences(of: "s", with: "✌️")
             let new_answer = user_answer_after ?? ""
-            print("あなた: \(new_answer)を出しました。")
+            print("You: \(new_answer)")
         
         case "e":
-            print("プログラムを終了します。")
+            print("Exit the program.")
             exit(0)
         default:
-            print("入力した値が合っていません。✊(r)✋(p)✌️(s)を選択してください。")
+            print("The values ​​you entered do not match. Select ✊(r) ✋(p) ✌️(s).")
             return Game_run()
         }
         
         
         let RPS = ["✊", "✋", "✌️"]
         let swift_answer = RPS.randomElement()
-        print("SWIFT: \(swift_answer ?? "")を出しました。")
+        print("SWIFT: \(swift_answer ?? "")")
         
         
         switch [user_answer_before, swift_answer] {
@@ -73,9 +73,10 @@ class RPS_Game {
         case ["s", "✊"]:
             print("Your defeat...")
             
-        // 未知じゃね????
+        // ????
         default:
-            print("知らんよ")
+            print("error: ????")
+            exit(0)
         }
         
         return Game_run()
@@ -83,18 +84,18 @@ class RPS_Game {
     
     // Game_run StartFunc()
     func start() {
-        print(">> RPS_Game (開始:s / 終了:e)")
+        print(">> RPS_Game (Start: s / End: e)")
         let Text:String = readLine() ?? ""
         
         switch Text {
             case "s":
-                print("ゲームを開始します。")
+                print("Start the game.")
                 Game_run()
             case "e":
-                print("プログラムを終了します。")
+                print("Exit the program.")
                 exit(0)
             default:
-                print(">> 入力した値が合っていません、どちらかを選択してください。")
+                print(">> The values ​​you entered do not match. Please select one.")
                 return start()
         }
     }
